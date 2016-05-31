4be51b58f300b4a37376157e5ef179f2bb3d2a2e
Peter Lemiszki <plemiszki@gmail.com>
2016-05-31 12:14:15 -0400

    first commit

---
+++ db/migrate/20160531155619_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email, null: false, unique: true
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :name, null: false
+      t.text :bio
+      t.string :species, null: false
+      t.string :location, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
