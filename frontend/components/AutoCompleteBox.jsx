9f3d3e2c2993ebfdc10eaa5f19caff09cf0d7d95
Peter Lemiszki <plemiszki@gmail.com>
2016-06-17 18:44:59 -0400

    cleaned up AutoCompleteBox

--- frontend/components/AutoCompleteBox.jsx
+++ frontend/components/AutoCompleteBox.jsx
@@ -18,29 +18,24 @@ var AutoCompleteBox = React.createClass({
   },
 
   initAutocomplete: function() {
-      // Create the autocomplete object, restricting the search to geographical
-      // location types.
-      autocomplete = new google.maps.places.Autocomplete(
-          /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
-          {types: ['geocode']});
+    autocomplete = new google.maps.places.Autocomplete(
+      (document.getElementById('autocomplete')),
+      {types: ['geocode']}
+    );
+    autocomplete.addListener('place_changed', this.fillInAddress);
+  },
 
-      // When the user selects an address from the dropdown, populate the address
-      // fields in the form.
-      autocomplete.addListener('place_changed', this.fillInAddress);
-    },
-
-    fillInAddress: function () {
-      // Get the place details from the autocomplete object.
-      var place = autocomplete.getPlace();
-      this.setState({
-        number: place.address_components[0].long_name,
-        street: place.address_components[1].long_name,
-        city: place.address_components[4].long_name,
-        state: place.address_components[6].long_name,
-        country: place.address_components[7].long_name,
-        zip: place.address_components[8].long_name
-      });
-    },
+  fillInAddress: function () {
+    var place = autocomplete.getPlace();
+    this.setState({
+      number: place.address_components[0].long_name,
+      street: place.address_components[1].long_name,
+      city: place.address_components[4].long_name,
+      state: place.address_components[6].long_name,
+      country: place.address_components[7].long_name,
+      zip: place.address_components[8].long_name
+    });
+  },
 
   render: function () {
     return(
